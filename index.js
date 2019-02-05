77af075ad56d60b6de9e784f1c4027687567da98
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-02-05 11:27:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function destructivelyRemoveFirstKitten(name) {
 }
   
 function appendKitten(name) {
-  return appendKitten(name);
+  return kittens = [...kittens, ''];
+  return kittens;
 }
 
 function prependKitten(name) {
@@ -33,4 +34,7 @@ function removeFirstKitten() {
   return kittens.slice(1);
 }
 
-
+function addElementToEndOfArray(array, element) {
+  array = [...array, 'foo']
+  return array;
+}
