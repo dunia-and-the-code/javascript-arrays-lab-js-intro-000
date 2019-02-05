98f4679a1b078bd5ff60ef9bfb98f25f9a4e18cb
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-02-05 11:25:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+}
+  
+function appendKitten(name) {
+  return appendKitten(name);
+}
+
+function prependKitten(name) {
+  return prependKitten(name);
+}
+
+function removeLastKitten() {
+  return kittens.slice(-1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
+
+
